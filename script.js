75690dd3aeb9a9e31ede52677ffc78a70f579d4f
charles-lao <charles.h.lao@gmail.com>
2022-11-12 01:39:43 +0800

    Create basic math functions

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+console.log(add(1, 2));
+console.log(subtract(1, 2));
+console.log(multiply(1, 2));
+console.log(divide(1, 2));
