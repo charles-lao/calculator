9b32791d8f18231df2030fcb6a564328d10d5dc6
charles-lao <charles.h.lao@gmail.com>
2022-11-15 03:14:24 +0800

    Create functions to store and update display value

--- script.js
+++ script.js
@@ -15,21 +15,47 @@ function divide(x, y) {
 }
 
 function operate(operator, x, y) {
-    
+    alert(x+' '+operator+ ' '+y);
 }
 
 function clearDisplay() {
     display.textContent = '';
+    currDisplayVal = '';
 }
+
+let currDisplayVal;
+let currOperation;
+let x;
+let y;
+
 
 const display = document.querySelector("#display");
 
 const clearBtn = document.querySelector("#clear-btn");
 clearBtn.addEventListener('click', clearDisplay);
 
-//display.textContent = '0';
+const numBtns = document.querySelectorAll(".grey-btns");
+numBtns.forEach((numBtn) => {
+    numBtn.addEventListener('click', () => {
+        display.textContent += numBtn.value;
+        currDisplayVal = display.textContent;
+    });
+});
 
-console.log(add(1, 2));
-console.log(subtract(1, 2));
-console.log(multiply(1, 2));
-console.log(divide(1, 2));
+const operateBtns = document.querySelectorAll(".orange-btns");
+operateBtns.forEach((operateBtn) => {
+    operateBtn.addEventListener('click', () => {
+
+        let operation = operateBtn.value 
+        if(operation != '='){
+            x = currDisplayVal;
+            currOperation = operation;
+            clearDisplay();
+        } else {
+            y = currDisplayVal;
+            clearDisplay();
+            operate(currOperation, x, y);
+
+        }
+    });
+});
