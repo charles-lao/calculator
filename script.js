0a3199859e230e79daaff1ec98f95553664f4a79
charles-lao <charles.h.lao@gmail.com>
2022-11-15 23:33:45 +0800

    Fix error when attempting multiple operations

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+let currDisplayVal;
+let currOperation;
+let x;
+let y;
+let storedVal;
+let prevOperation;
+
 function add(x, y) {
     return x + y;
 }
@@ -18,25 +25,24 @@ function operate(operator, x, y) {
     switch(operator) {
         case '+':
             updateDisplay(add(x, y));
-            storedVal = currDisplayVal;
-            resetValues();
+            storedVal = add(x, y);  
             break;
         case '-':
-            updateDisplay(subtract(x,y));
-            storedVal = currDisplayVal;
-            resetValues();
+            updateDisplay(subtract(x, y));
+            storedVal = subtract(x, y);     
             break;
-        case 'x':
-            updateDisplay(multiply(x,y));
-            storedVal = currDisplayVal;
-            resetValues();
+        case 'X':
+            updateDisplay(multiply(x, y));
+            storedVal = multiply(x, y);         
             break;
         case '/':
-            updateDisplay(divide(x,y));
-            storedVal = currDisplayVal;
-            resetValues();
+            updateDisplay(divide(x, y));
+            storedVal = divide(x, y);
             break;
         default:
+            alert("Error! Operator is "+operator);
+            clearDisplay();
+            resetValues();
             break;
     }
 }
@@ -55,25 +61,23 @@ function resetValues(){
     x = undefined;
     y = undefined;
     currOperation = undefined;
+    storedVal = undefined;
 }
-
-let currDisplayVal;
-let currOperation;
-let x;
-let y;
-let storedVal;
-
 
 const display = document.querySelector("#display");
 
 const clearBtn = document.querySelector("#clear-btn");
-clearBtn.addEventListener('click', clearDisplay, resetValues);
+clearBtn.addEventListener('click', () => {
+    clearDisplay();
+    resetValues();
+});
 
 const numBtns = document.querySelectorAll(".grey-btns");
 numBtns.forEach((numBtn) => {
     numBtn.addEventListener('click', () => {
         display.textContent += numBtn.value;
-        currDisplayVal = display.textContent;
+        currDisplayVal = Number(display.textContent);
+        
     });
 });
 
@@ -82,25 +86,29 @@ operateBtns.forEach((operateBtn) => {
     operateBtn.addEventListener('click', () => {
 
         let operation = operateBtn.value
+        currOperation = operation;
         if(operation !== '=' && x === undefined){
-            x = Number(currDisplayVal);
-            currOperation = operation;
+            x = currDisplayVal;
             clearDisplay();
+            prevOperation = currOperation;
         } else if (operation !== '=' && y === undefined) {
-            y = Number(currDisplayVal);
-            operate(currOperation, x, y);
+            y = currDisplayVal;
+            operate(prevOperation, x, y);
+            prevOperation = currOperation;
             clearDisplay();
-        } else if (operation !== '=' && storedVal !== undefined) {
+        } else if (operation !== '=' && x !== undefined && y !== undefined) {
             x = storedVal;
-            y = Number(currDisplayVal);
-            operate(currOperation, x, y);
+            y = currDisplayVal;
+            operate(prevOperation, x, y);
+            prevOperation = operation;
             clearDisplay();
-            alert(storedVal);
         } else if (operation === '=') {
-            //alert('Equals is pressed');
-            y = Number(currDisplayVal);
-            operate(currOperation, x, y);
-            
+            if(storedVal !== undefined){
+                x = storedVal;
+            }
+            y = currDisplayVal;
+            operate(prevOperation, x, y);
+            resetValues();
         }
     });
 });
