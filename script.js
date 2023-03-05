0413edc7a689d9c5ae90c2fb09cf508575e2cbb7
Charles Lao <charles.h.lao@gmail.com>
2023-03-05 17:17:08 +0800

    Add eslint and prettier to the project

--- script.js
+++ script.js
@@ -5,220 +5,205 @@ let y;
 let storedVal;
 let prevOperation;
 
-function add(x, y) {
-    return decimalFilter(x+y);
+const display = document.querySelector('#display');
+
+// checks if num is whole number and appends decimals if false
+function decimalFilter(result) {
+  if ((result % 1) === 0) {
+    return result;
+  }
+  return result.toFixed(2);
 }
 
-function subtract(x, y) {
-    return decimalFilter(x-y);
-}
-
-function multiply(x, y) {
-    return decimalFilter(x * y);
-}
-
-function modulo(x, y) {
-    return (x % y);
-}
-
-function divide(x, y) {
-    return decimalFilter(x / y);
-}
-
-function operate(operator, x, y) {
-    switch(operator) {
-        case '+':
-            updateDisplay(add(x, y));
-            storedVal = add(x, y);  
-            break;
-        case '-':
-            updateDisplay(subtract(x, y));
-            storedVal = subtract(x, y);     
-            break;
-        case 'x':
-            updateDisplay(multiply(x, y));
-            storedVal = multiply(x, y);         
-            break;
-        case '*':
-            updateDisplay(multiply(x, y));
-            storedVal = multiply(x, y);         
-            break;
-        case '%':
-            updateDisplay(modulo(x, y));
-            storedVal = modulo(x, y);         
-            break;
-        case '/':
-            if(y === 0){
-                alert("ERROR! Unable to divide by Zero. Please try again.");
-                clearDisplay();
-                resetValues();
-                break;
-            }
-            updateDisplay(divide(x, y));
-            storedVal = divide(x, y);
-            break;
-        default:
-            alert("Error! Operator is "+operator);
-            clearDisplay();
-            resetValues();
-            break;
-    }
-}
-
-function inputNumber(numKey){
-
-    //does a period check
-    if(numKey == '.'){
-        if(periodFilter(display.textContent)){
-            //do nothing if there is a period already
-        } else {
-            display.textContent += numKey;
-            currDisplayVal = Number(display.textContent);
-        }
-    } else {
-        display.textContent += numKey;
-        currDisplayVal = Number(display.textContent);
-    }
-}
-
-function startOperation(operation) {
-
-    currOperation = operation;
-    if(operation !== '=' && x === undefined){
-        x = currDisplayVal;
-        clearDisplay();
-        prevOperation = currOperation;
-    } else if (operation !== '=' && y === undefined) {
-        y = currDisplayVal;
-        operate(prevOperation, x, y);
-        prevOperation = currOperation;
-        clearDisplay();
-    } else if (operation !== '=' && x !== undefined && y !== undefined) {
-        x = storedVal;
-        y = currDisplayVal;
-        operate(prevOperation, x, y);
-        prevOperation = operation;
-        clearDisplay();
-    } else if (operation === '=') {
-        if(storedVal !== undefined){
-            x = storedVal;
-        }
-        y = currDisplayVal;
-        operate(prevOperation, x, y);
-        resetValues();
-    }
-
+function updateDisplay(value) {
+  currDisplayVal = value;
+  display.textContent = value;
 }
 
 function clearDisplay() {
-    display.textContent = '';
-    currDisplayVal = '';
+  display.textContent = '';
+  currDisplayVal = '';
 }
 
-function updateDisplay(value){
-    currDisplayVal = value;
-    display.textContent = value;
+function resetValues() {
+  x = undefined;
+  y = undefined;
+  currOperation = undefined;
+  storedVal = undefined;
+  prevOperation = undefined;
 }
 
-function resetValues(){
-    x = undefined;
-    y = undefined;
-    currOperation = undefined;
-    storedVal = undefined;
-    prevOperation = undefined;
-}
+// checks if there's a period on the display already
+function periodFilter(string) {
+  if (currDisplayVal !== undefined) {
+    string = string.toString();
 
-//checks if num is whole number and appends decimals if false
-function decimalFilter(result){
-
-    if((result%1) === 0){
-        return result
-    } else {
-        return result.toFixed(2);
+    if (string.includes('.')) {
+      return true;
     }
+    return false;
+  }
 }
 
-//checks if there's a period on the display already
-function periodFilter(string){
-
-    if(currDisplayVal !== undefined){
-        string = string.toString();
-
-        if(string.includes('.')){
-            return true;
-        } else {
-            false;
-        }
-    }   
+function add(a, b) {
+  return decimalFilter(a + b);
 }
 
-//delete last character from string
-function backSpace() {
-
-    if(currDisplayVal !== undefined && display.textContent !== undefined) {
-        let screenDisplay = display.textContent;
-
-        currDisplayVal = currDisplayVal.toString();
-        currDisplayVal = Number(currDisplayVal.slice(0, -1));
-        display.textContent = screenDisplay.slice(0, -1);
-    } 
+function subtract(a, b) {
+  return decimalFilter(a - b);
 }
 
-const display = document.querySelector("#display");
+function multiply(a, b) {
+  return decimalFilter(a * b);
+}
 
-const clearBtn = document.querySelector("#clear-btn");
-clearBtn.addEventListener('click', () => {
+function modulo(a, b) {
+  return (a % b);
+}
+
+function divide(a, b) {
+  return decimalFilter(a / b);
+}
+
+function operate(operator, a, b) {
+  switch (operator) {
+    case '+':
+      updateDisplay(add(a, b));
+      storedVal = add(a, b);
+      break;
+    case '-':
+      updateDisplay(subtract(a, b));
+      storedVal = subtract(a, b);
+      break;
+    case 'x':
+      updateDisplay(multiply(a, b));
+      storedVal = multiply(a, b);
+      break;
+    case '*':
+      updateDisplay(multiply(a, b));
+      storedVal = multiply(a, b);
+      break;
+    case '%':
+      updateDisplay(modulo(a, b));
+      storedVal = modulo(a, b);
+      break;
+    case '/':
+      if (b === 0) {
+        alert('ERROR! Unable to divide by Zero. Please try again.');
+        clearDisplay();
+        resetValues();
+        break;
+      }
+      updateDisplay(divide(a, b));
+      storedVal = divide(a, b);
+      break;
+    default:
+      alert(`Error! Operator is ${operator}`);
+      clearDisplay();
+      resetValues();
+      break;
+  }
+}
+
+function inputNumber(numKey) {
+  // does a period check
+  if (numKey == '.') {
+    if (periodFilter(display.textContent)) {
+      // do nothing if there is a period already
+    } else {
+      display.textContent += numKey;
+      currDisplayVal = Number(display.textContent);
+    }
+  } else {
+    display.textContent += numKey;
+    currDisplayVal = Number(display.textContent);
+  }
+}
+
+function startOperation(operation) {
+  currOperation = operation;
+  if (operation !== '=' && x === undefined) {
+    x = currDisplayVal;
     clearDisplay();
+    prevOperation = currOperation;
+  } else if (operation !== '=' && y === undefined) {
+    y = currDisplayVal;
+    operate(prevOperation, x, y);
+    prevOperation = currOperation;
+    clearDisplay();
+  } else if (operation !== '=' && x !== undefined && y !== undefined) {
+    x = storedVal;
+    y = currDisplayVal;
+    operate(prevOperation, x, y);
+    prevOperation = operation;
+    clearDisplay();
+  } else if (operation === '=') {
+    if (storedVal !== undefined) {
+      x = storedVal;
+    }
+    y = currDisplayVal;
+    operate(prevOperation, x, y);
     resetValues();
+  }
+}
+
+// delete last character from string
+function backSpace() {
+  if (currDisplayVal !== undefined && display.textContent !== undefined) {
+    const screenDisplay = display.textContent;
+
+    currDisplayVal = currDisplayVal.toString();
+    currDisplayVal = Number(currDisplayVal.slice(0, -1));
+    display.textContent = screenDisplay.slice(0, -1);
+  }
+}
+
+const clearBtn = document.querySelector('#clear-btn');
+clearBtn.addEventListener('click', () => {
+  clearDisplay();
+  resetValues();
 });
 
-//backspace btn
+// backspace btn
 const delBtn = document.querySelector('#del-btn');
 delBtn.addEventListener('click', () => {
-    
-    backSpace();
-     
+  backSpace();
 });
 
-const numBtns = document.querySelectorAll(".grey-btns");
+const numBtns = document.querySelectorAll('.grey-btns');
 numBtns.forEach((numBtn) => {
-    numBtn.addEventListener('click', () => {
-
-        inputNumber(numBtn.value);
-        
-    });
+  numBtn.addEventListener('click', () => {
+    inputNumber(numBtn.value);
+  });
 });
 
-const operateBtns = document.querySelectorAll(".operator-btns");
+const operateBtns = document.querySelectorAll('.operator-btns');
 operateBtns.forEach((operateBtn) => {
-    operateBtn.addEventListener('click', () => {
-
-        startOperation(operateBtn.value);
-    });
+  operateBtn.addEventListener('click', () => {
+    startOperation(operateBtn.value);
+  });
 });
 
-window.addEventListener('keydown', function (e) {
+window.addEventListener('keydown', (e) => {
+  const validOperators = ['+', '-', '*', 'x', '=', 'Enter', '/', '%'];
 
-    const validOperators = ['+', '-', '*', 'x', '=', 'Enter', '/', '%' ];
+  // check if keyboard key pressed is a number
+  if (!isNaN(parseInt(e.key))) {
+    inputNumber(e.key);
+  } else if (e.key == '.') {
+    inputNumber(e.key);
+  }
 
-    //check if keyboard key pressed is a number
-    if (!isNaN(parseInt(e.key))) {
-        inputNumber(e.key);
-    } else if (e.key == '.') {
-        inputNumber(e.key);
-    } 
-    
-    //check if key pressed is a valid math operator
-    if (validOperators.includes(e.key)) {
-        if(e.key == 'Enter'){
-            startOperation('=');
-        }else {
-            startOperation(e.key);
-        }
+  // check if key pressed is a valid math operator
+  if (validOperators.includes(e.key)) {
+    if (e.key == 'Enter') {
+      startOperation('=');
+    } else {
+      startOperation(e.key);
     }
+  }
 
-    if (e.key === 'Backspace') {
-        backSpace();
-    }
-
+  if (e.key === 'Backspace') {
+    backSpace();
+  }
 });
